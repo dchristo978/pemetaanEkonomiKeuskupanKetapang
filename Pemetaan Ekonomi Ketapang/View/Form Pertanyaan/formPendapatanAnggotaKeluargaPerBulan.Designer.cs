16594286ecde065ae87d94b6320dcfb85bd6e48c
dchristo978 <email@example.com>
2019-09-30 20:43:50 +0700

    Form Pendapatan Anggota Keluarga Belum Selesai

---
+++ Pemetaan Ekonomi Ketapang/View/Form Pertanyaan/formPendapatanAnggotaKeluargaPerBulan.Designer.cs
@@ -0,0 +1,405 @@
+﻿namespace Pemetaan_Ekonomi_Ketapang.View.Form_Pertanyaan
+{
+    partial class formPendapatanAnggotaKeluargaPerBulan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.lb_stasi = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.lb_paroki = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.lb_umur = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.lb_jenisKelamin = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.lb_nok5 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.lb_namaKK = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel7 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel8 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel9 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel10 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel11 = new MetroFramework.Controls.MetroLabel();
+            this.edtSuami = new System.Windows.Forms.TextBox();
+            this.edtIstri = new System.Windows.Forms.TextBox();
+            this.edtAnak = new System.Windows.Forms.TextBox();
+            this.lbTotal = new System.Windows.Forms.Label();
+            this.cmbSimpan = new MetroFramework.Controls.MetroButton();
+            this.metroLabel12 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel13 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel14 = new MetroFramework.Controls.MetroLabel();
+            this.cbSuami = new System.Windows.Forms.CheckBox();
+            this.cbIstri = new System.Windows.Forms.CheckBox();
+            this.cbAnak = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.metroLabel4);
+            this.groupBox1.Controls.Add(this.lb_stasi);
+            this.groupBox1.Controls.Add(this.metroLabel1);
+            this.groupBox1.Controls.Add(this.lb_paroki);
+            this.groupBox1.Controls.Add(this.metroLabel2);
+            this.groupBox1.Controls.Add(this.lb_umur);
+            this.groupBox1.Controls.Add(this.metroLabel3);
+            this.groupBox1.Controls.Add(this.lb_jenisKelamin);
+            this.groupBox1.Controls.Add(this.metroLabel5);
+            this.groupBox1.Controls.Add(this.lb_nok5);
+            this.groupBox1.Controls.Add(this.metroLabel6);
+            this.groupBox1.Controls.Add(this.lb_namaKK);
+            this.groupBox1.Location = new System.Drawing.Point(44, 81);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(1181, 191);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Identitas";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(719, 48);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(59, 20);
+            this.metroLabel4.TabIndex = 3;
+            this.metroLabel4.Text = "4. Umur";
+            // 
+            // lb_stasi
+            // 
+            this.lb_stasi.AutoSize = true;
+            this.lb_stasi.Location = new System.Drawing.Point(833, 128);
+            this.lb_stasi.Name = "lb_stasi";
+            this.lb_stasi.Size = new System.Drawing.Size(111, 20);
+            this.lb_stasi.TabIndex = 11;
+            this.lb_stasi.Text = "Stasi taruh di sini";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(27, 48);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(161, 20);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "1. Nama Kepala Keluarga";
+            // 
+            // lb_paroki
+            // 
+            this.lb_paroki.AutoSize = true;
+            this.lb_paroki.Location = new System.Drawing.Point(833, 90);
+            this.lb_paroki.Name = "lb_paroki";
+            this.lb_paroki.Size = new System.Drawing.Size(121, 20);
+            this.lb_paroki.TabIndex = 10;
+            this.lb_paroki.Text = "Paroki taruh di sini";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(27, 90);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(98, 20);
+            this.metroLabel2.TabIndex = 1;
+            this.metroLabel2.Text = "2. No Identitas";
+            // 
+            // lb_umur
+            // 
+            this.lb_umur.AutoSize = true;
+            this.lb_umur.Location = new System.Drawing.Point(833, 48);
+            this.lb_umur.Name = "lb_umur";
+            this.lb_umur.Size = new System.Drawing.Size(161, 20);
+            this.lb_umur.TabIndex = 9;
+            this.lb_umur.Text = "Tanggal lahir taruh di sini";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(27, 128);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(107, 20);
+            this.metroLabel3.TabIndex = 2;
+            this.metroLabel3.Text = "3. Jenis Kelamin";
+            // 
+            // lb_jenisKelamin
+            // 
+            this.lb_jenisKelamin.AutoSize = true;
+            this.lb_jenisKelamin.Location = new System.Drawing.Point(276, 128);
+            this.lb_jenisKelamin.Name = "lb_jenisKelamin";
+            this.lb_jenisKelamin.Size = new System.Drawing.Size(182, 20);
+            this.lb_jenisKelamin.TabIndex = 8;
+            this.lb_jenisKelamin.Text = "Jenis Kelamin di taruh di sini";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(719, 90);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(61, 20);
+            this.metroLabel5.TabIndex = 4;
+            this.metroLabel5.Text = "5. Paroki";
+            // 
+            // lb_nok5
+            // 
+            this.lb_nok5.AutoSize = true;
+            this.lb_nok5.Location = new System.Drawing.Point(276, 90);
+            this.lb_nok5.Name = "lb_nok5";
+            this.lb_nok5.Size = new System.Drawing.Size(192, 20);
+            this.lb_nok5.TabIndex = 7;
+            this.lb_nok5.Text = "No K5 Keluarga ditaruh di sini";
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.Location = new System.Drawing.Point(719, 128);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(51, 20);
+            this.metroLabel6.TabIndex = 5;
+            this.metroLabel6.Text = "6. Stasi";
+            // 
+            // lb_namaKK
+            // 
+            this.lb_namaKK.AutoSize = true;
+            this.lb_namaKK.Location = new System.Drawing.Point(276, 47);
+            this.lb_namaKK.Name = "lb_namaKK";
+            this.lb_namaKK.Size = new System.Drawing.Size(237, 20);
+            this.lb_namaKK.TabIndex = 6;
+            this.lb_namaKK.Text = "Nama Kepala Keluarga Ditaruh di sini";
+            // 
+            // metroLabel7
+            // 
+            this.metroLabel7.AutoSize = true;
+            this.metroLabel7.Location = new System.Drawing.Point(490, 297);
+            this.metroLabel7.Name = "metroLabel7";
+            this.metroLabel7.Size = new System.Drawing.Size(295, 20);
+            this.metroLabel7.TabIndex = 17;
+            this.metroLabel7.Text = "Total pendapatan anggota keluarga per bulan :";
+            // 
+            // metroLabel8
+            // 
+            this.metroLabel8.AutoSize = true;
+            this.metroLabel8.Location = new System.Drawing.Point(464, 347);
+            this.metroLabel8.Name = "metroLabel8";
+            this.metroLabel8.Size = new System.Drawing.Size(73, 20);
+            this.metroLabel8.TabIndex = 18;
+            this.metroLabel8.Text = "A. Suami : ";
+            // 
+            // metroLabel9
+            // 
+            this.metroLabel9.AutoSize = true;
+            this.metroLabel9.Location = new System.Drawing.Point(464, 389);
+            this.metroLabel9.Name = "metroLabel9";
+            this.metroLabel9.Size = new System.Drawing.Size(56, 20);
+            this.metroLabel9.TabIndex = 19;
+            this.metroLabel9.Text = "B. Istri : ";
+            // 
+            // metroLabel10
+            // 
+            this.metroLabel10.AutoSize = true;
+            this.metroLabel10.Location = new System.Drawing.Point(464, 431);
+            this.metroLabel10.Name = "metroLabel10";
+            this.metroLabel10.Size = new System.Drawing.Size(67, 20);
+            this.metroLabel10.TabIndex = 20;
+            this.metroLabel10.Text = "C. Anak : ";
+            // 
+            // metroLabel11
+            // 
+            this.metroLabel11.AutoSize = true;
+            this.metroLabel11.Location = new System.Drawing.Point(483, 485);
+            this.metroLabel11.Name = "metroLabel11";
+            this.metroLabel11.Size = new System.Drawing.Size(48, 20);
+            this.metroLabel11.TabIndex = 21;
+            this.metroLabel11.Text = "Total : ";
+            // 
+            // edtSuami
+            // 
+            this.edtSuami.Location = new System.Drawing.Point(604, 347);
+            this.edtSuami.Name = "edtSuami";
+            this.edtSuami.Size = new System.Drawing.Size(207, 22);
+            this.edtSuami.TabIndex = 22;
+            this.edtSuami.TextChanged += new System.EventHandler(this.edtSuami_TextChanged);
+            // 
+            // edtIstri
+            // 
+            this.edtIstri.Location = new System.Drawing.Point(604, 389);
+            this.edtIstri.Name = "edtIstri";
+            this.edtIstri.Size = new System.Drawing.Size(207, 22);
+            this.edtIstri.TabIndex = 23;
+            this.edtIstri.TextChanged += new System.EventHandler(this.edtIstri_TextChanged);
+            // 
+            // edtAnak
+            // 
+            this.edtAnak.Location = new System.Drawing.Point(604, 431);
+            this.edtAnak.Name = "edtAnak";
+            this.edtAnak.Size = new System.Drawing.Size(207, 22);
+            this.edtAnak.TabIndex = 24;
+            this.edtAnak.TextChanged += new System.EventHandler(this.edtAnak_TextChanged);
+            // 
+            // lbTotal
+            // 
+            this.lbTotal.AutoSize = true;
+            this.lbTotal.Location = new System.Drawing.Point(567, 488);
+            this.lbTotal.Name = "lbTotal";
+            this.lbTotal.Size = new System.Drawing.Size(274, 17);
+            this.lbTotal.TabIndex = 25;
+            this.lbTotal.Text = "Total pendapatan akan di tampilkan di sini";
+            // 
+            // cmbSimpan
+            // 
+            this.cmbSimpan.Location = new System.Drawing.Point(535, 623);
+            this.cmbSimpan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbSimpan.Name = "cmbSimpan";
+            this.cmbSimpan.Size = new System.Drawing.Size(216, 71);
+            this.cmbSimpan.TabIndex = 82;
+            this.cmbSimpan.Text = "Simpan";
+            this.cmbSimpan.UseSelectable = true;
+            this.cmbSimpan.Click += new System.EventHandler(this.cmbSimpan_Click);
+            // 
+            // metroLabel12
+            // 
+            this.metroLabel12.AutoSize = true;
+            this.metroLabel12.Location = new System.Drawing.Point(570, 349);
+            this.metroLabel12.Name = "metroLabel12";
+            this.metroLabel12.Size = new System.Drawing.Size(28, 20);
+            this.metroLabel12.TabIndex = 83;
+            this.metroLabel12.Text = "Rp.";
+            // 
+            // metroLabel13
+            // 
+            this.metroLabel13.AutoSize = true;
+            this.metroLabel13.Location = new System.Drawing.Point(570, 389);
+            this.metroLabel13.Name = "metroLabel13";
+            this.metroLabel13.Size = new System.Drawing.Size(28, 20);
+            this.metroLabel13.TabIndex = 84;
+            this.metroLabel13.Text = "Rp.";
+            // 
+            // metroLabel14
+            // 
+            this.metroLabel14.AutoSize = true;
+            this.metroLabel14.Location = new System.Drawing.Point(570, 431);
+            this.metroLabel14.Name = "metroLabel14";
+            this.metroLabel14.Size = new System.Drawing.Size(28, 20);
+            this.metroLabel14.TabIndex = 85;
+            this.metroLabel14.Text = "Rp.";
+            // 
+            // cbSuami
+            // 
+            this.cbSuami.AutoSize = true;
+            this.cbSuami.Location = new System.Drawing.Point(823, 350);
+            this.cbSuami.Name = "cbSuami";
+            this.cbSuami.Size = new System.Drawing.Size(18, 17);
+            this.cbSuami.TabIndex = 86;
+            this.cbSuami.UseVisualStyleBackColor = true;
+            this.cbSuami.CheckedChanged += new System.EventHandler(this.cbSuami_CheckedChanged);
+            // 
+            // cbIstri
+            // 
+            this.cbIstri.AutoSize = true;
+            this.cbIstri.Location = new System.Drawing.Point(823, 394);
+            this.cbIstri.Name = "cbIstri";
+            this.cbIstri.Size = new System.Drawing.Size(18, 17);
+            this.cbIstri.TabIndex = 87;
+            this.cbIstri.UseVisualStyleBackColor = true;
+            this.cbIstri.CheckedChanged += new System.EventHandler(this.cbIstri_CheckedChanged);
+            // 
+            // cbAnak
+            // 
+            this.cbAnak.AutoSize = true;
+            this.cbAnak.Location = new System.Drawing.Point(823, 434);
+            this.cbAnak.Name = "cbAnak";
+            this.cbAnak.Size = new System.Drawing.Size(18, 17);
+            this.cbAnak.TabIndex = 88;
+            this.cbAnak.UseVisualStyleBackColor = true;
+            this.cbAnak.CheckedChanged += new System.EventHandler(this.cbAnak_CheckedChanged);
+            // 
+            // formPendapatanAnggotaKeluargaPerBulan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1275, 778);
+            this.Controls.Add(this.cbAnak);
+            this.Controls.Add(this.cbIstri);
+            this.Controls.Add(this.cbSuami);
+            this.Controls.Add(this.metroLabel14);
+            this.Controls.Add(this.metroLabel13);
+            this.Controls.Add(this.metroLabel12);
+            this.Controls.Add(this.cmbSimpan);
+            this.Controls.Add(this.lbTotal);
+            this.Controls.Add(this.edtAnak);
+            this.Controls.Add(this.edtIstri);
+            this.Controls.Add(this.edtSuami);
+            this.Controls.Add(this.metroLabel11);
+            this.Controls.Add(this.metroLabel10);
+            this.Controls.Add(this.metroLabel9);
+            this.Controls.Add(this.metroLabel8);
+            this.Controls.Add(this.metroLabel7);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "formPendapatanAnggotaKeluargaPerBulan";
+            this.Text = "Form Pendapatan Anggota Keluarga Per Bulan";
+            this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
+            this.Load += new System.EventHandler(this.formPendapatanAnggotaKeluargaPerBulan_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroLabel lb_stasi;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel lb_paroki;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel lb_umur;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel lb_jenisKelamin;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroLabel lb_nok5;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Controls.MetroLabel lb_namaKK;
+        private MetroFramework.Controls.MetroLabel metroLabel7;
+        private MetroFramework.Controls.MetroLabel metroLabel8;
+        private MetroFramework.Controls.MetroLabel metroLabel9;
+        private MetroFramework.Controls.MetroLabel metroLabel10;
+        private MetroFramework.Controls.MetroLabel metroLabel11;
+        private System.Windows.Forms.TextBox edtSuami;
+        private System.Windows.Forms.TextBox edtIstri;
+        private System.Windows.Forms.TextBox edtAnak;
+        private System.Windows.Forms.Label lbTotal;
+        private MetroFramework.Controls.MetroButton cmbSimpan;
+        private MetroFramework.Controls.MetroLabel metroLabel12;
+        private MetroFramework.Controls.MetroLabel metroLabel13;
+        private MetroFramework.Controls.MetroLabel metroLabel14;
+        private System.Windows.Forms.CheckBox cbSuami;
+        private System.Windows.Forms.CheckBox cbIstri;
+        private System.Windows.Forms.CheckBox cbAnak;
+    }
+}
---
+++ Pemetaan Ekonomi Ketapang/View/Form Pertanyaan/formPendapatanAnggotaKeluargaPerBulan.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using Pemetaan_Ekonomi_Ketapang.Controller.Umat;
+using Pemetaan_Ekonomi_Ketapang.Model;
+using Pemetaan_Ekonomi_Ketapang.Controller.Ekonomi;
+using Pemetaan_Ekonomi_Ketapang.Controller.Global;
+using Pemetaan_Ekonomi_Ketapang.Controller.Umat;
+using Pemetaan_Ekonomi_Ketapang.Controller;
+using System.Diagnostics;
+
+namespace Pemetaan_Ekonomi_Ketapang.View.Form_Pertanyaan
+{
+    public partial class formPendapatanAnggotaKeluargaPerBulan : MetroForm
+    {
+        private UmatControl umatControl = new UmatControl();
+        private StasiControl stasiControl = new StasiControl();
+        private ParokiControl parokiControl = new ParokiControl();
+        private Ref_JawabanControl refJawabanControl = new Ref_JawabanControl();
+        private Tbl_JawabanControl tblJawaanControl = new Tbl_JawabanControl();
+
+        public formPendapatanAnggotaKeluargaPerBulan()
+        {
+            InitializeComponent();
+        }
+
+        private void initializeHeader()
+        {
+            tbl_umat umat = umatControl.getDataUmatSatuan(Convert.ToInt32(formIdentitas.idUmat));
+
+            Debug.Write("ISI NOMOR IDENTIAS : " + " KTP " + umat.no_ktp + " KK " + umat.no_kk + "K5 " + umat.no_k5);
+
+            lb_namaKK.Text = umat.nama;
+            lb_nok5.Text = umatControl.getNomorIdentitas(umat);
+            lb_umur.Text = umat.umur.ToString() + " Tahun";
+            lb_stasi.Text = stasiControl.getNamaStasi(umat.id_stasi);
+            lb_paroki.Text = parokiControl.getNamaParokiByID(umat.id_paroki);
+            lb_jenisKelamin.Text = umat.jenis_kelamin;
+        }
+
+        private void formPendapatanAnggotaKeluargaPerBulan_Load(object sender, EventArgs e)
+        {
+            initializeHeader();
+        }
+
+        private void cbSuami_CheckedChanged(object sender, EventArgs e)
+        {
+            this.edtSuami.Enabled = this.cbSuami.Checked;
+            this.edtSuami.Text = "";
+        }
+
+        private void cbIstri_CheckedChanged(object sender, EventArgs e)
+        {
+            this.edtIstri.Enabled = this.cbIstri.Checked;
+            this.edtIstri.Text = "";
+        }
+
+        private void cbAnak_CheckedChanged(object sender, EventArgs e)
+        {
+            this.edtAnak.Enabled = this.cbAnak.Checked;
+            this.edtAnak.Text = "";
+        }
+
+        private void cmbSimpan_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrWhiteSpace(this.edtSuami.Text) && !String.IsNullOrWhiteSpace(this.edtIstri.Text) && !String.IsNullOrWhiteSpace(this.edtAnak.Text))
+            {
+                MessageBox.Show("Pastikan salah satu data sudah diisi sesuai dengan form yang ada!");
+            }
+            else
+            {
+                try
+                {
+
+                    MessageBox.Show("Data berhasil di masukan, silahkan lanjutkan ke form berikutnya !");
+                    this.Close();
+                }
+                catch(Exception E)
+                {
+                    MessageBox.Show("Terjadi kesalahan pada sistem, silahkan hubungi admin dengan menyertakan kalimat berikut : " + E.Message);
+                }
+            }
+        }
+
+        private void checkTotal()
+        {
+            int temp = 0;
+
+            if (!String.IsNullOrWhiteSpace(this.edtSuami.Text))
+                temp += Convert.ToInt32(this.edtSuami.Text);
+
+            if (!String.IsNullOrWhiteSpace(this.edtIstri.Text))
+                temp += Convert.ToInt32(this.edtIstri.Text);
+
+            if (!String.IsNullOrWhiteSpace(this.edtAnak.Text))
+                temp += Convert.ToInt32(this.edtAnak.Text);
+
+            if (temp >= 0)
+                this.lbTotal.Text = "Rp." + temp.ToString();
+            else
+                this.lbTotal.Text = "Total akan ditampilkan di sini";
+        }
+
+        private void edtSuami_TextChanged(object sender, EventArgs e)
+        {
+            checkTotal();
+        }
+
+        private void edtIstri_TextChanged(object sender, EventArgs e)
+        {
+            checkTotal();
+        }
+
+        private void edtAnak_TextChanged(object sender, EventArgs e)
+        {
+            checkTotal();
+        }
+    }
+}
